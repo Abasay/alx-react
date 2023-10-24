b4f7187da9f21da37c511fffe74c97cc5e38ca7b
Abasay <abdulsalamasheem@gmail.com>
2023-10-24 14:20:43 +0000

    new

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -5,6 +5,7 @@ import _ from 'lodash';
 const body = $('body');
 let count = 0;
 
+$('body').append("<div id=logo></div>");
 body.append('<p>Holberton Dashboard</p>');
 body.append('<p>Dashboard data for the students</p>');
 body.append('<button>Click here to get started</button>');
