954474f782913e45c7fef83c2ead4103039d636f
Abasay <111005754+Abasay@users.noreply.github.com>
2023-10-25 14:34:03 +0100

    webpack-react

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,12 @@
-console.log('working')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Notifications } from './Notifications'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Notifications />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
