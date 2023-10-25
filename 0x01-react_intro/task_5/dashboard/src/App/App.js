f4fce03b762e663e7205897b4bbb5e9f506b5278
Abasay <111005754+Abasay@users.noreply.github.com>
2023-10-25 14:43:00 +0100

    sort the folders

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import logo from './holberton_logo.jpg'
+import logo from '../assets/holberton_logo.jpg'
 import './App.css'
-import { getFooterCopy, getFullYear } from './utils'
+import { getFooterCopy, getFullYear } from '../utils/utils'
 
 function App() {
   return (
