3de8a5daf840dd7e05991dde845cd017d55c4637
Abasay <111005754+Abasay@users.noreply.github.com>
2023-10-25 12:06:47 +0100

    package

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,13 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: '../src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: '../dist',
     hot: true,
     port: 8564,
   },
