7960d1891cd272318bdb887f925765edea095b64
Abasay <111005754+Abasay@users.noreply.github.com>
2023-11-02 00:46:01 +0100

    new

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -9,24 +9,24 @@ import PropTypes from 'prop-types'
 import { getLatestNotification } from '../utils/utils'
 
 export default class App extends React.Component {
+  static listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ]
+
+  static listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+  ]
   constructor(props) {
     super(props)
-    this.listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 },
-    ]
-
-    this.listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-    ]
   }
 
   render() {
     return (
-      <>
+      <React.Fragment>
         <Notifications listNotifications={this.listNotifications} />
         <div className='App'>
           <Header />
@@ -41,7 +41,7 @@ export default class App extends React.Component {
         <div className='App-footer'>
           <Footer />
         </div>
-      </>
+      </React.Fragment>
     )
   }
 }
